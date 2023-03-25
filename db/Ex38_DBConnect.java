5ecbedff67f940e56559981a64362471f86dc5cb
째이 <120344687+jjaei@users.noreply.github.com>
2023-03-25 17:40:58 +0900

    mariadb와 java 연동하기

---
+++ db/Ex38_DBConnect.java
@@ -0,0 +1,30 @@
+package day08.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Ex38_DBConnect {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Connection conn = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection(
+					"jdbc:mariadb://localhost:3399/member",
+					"root",
+					"1234"
+					);
+		} catch(Exception e) {
+			System.out.println("Exception Occurred");
+		}
+		
+		if(conn != null) {
+			System.out.println("Congratulations!");
+			System.out.println("Database Connection OK.");
+			System.out.println(conn);
+		}
+	}
+}
