090b35dc18bd8fb64711a389b6a5a4a7213d9b1d
째이 <120344687+jjaei@users.noreply.github.com>
2023-04-03 10:12:06 +0900

    MemberVO 작성

---
+++ Web/src/main/java/com/it/db/MemberVO.java
@@ -0,0 +1,17 @@
+package com.it.db;
+
+public class MemberVO {
+
+	private int memberno;
+	private String id;
+	private String name;
+	
+	public int getMemberNo() {return memberno;}
+	public String getId() {return id;}
+	public String getName() {return name;}
+	
+	public void setMemberNo(int memberno) {this.memberno = memberno;}
+	public void setId(String id) {this.id = id;}
+	public void setName(String name) {this.name = name;}
+	
+}
