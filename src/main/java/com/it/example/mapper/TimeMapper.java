fbadec18211087a1ce7af25bb2e6fd3bffe50fa9
jjaei <ieajnowk@gmail.com>
2023-04-15 18:24:48 +0900

    TimeMapper 인터페이스 생성

---
+++ src/main/java/com/it/example/mapper/TimeMapper.java
@@ -0,0 +1,10 @@
+package com.it.example.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Service;
+
+@Service  // 스프링에서 관리하는 Bean에 등록
+@Mapper // 마이배티스 연결
+public interface TimeMapper {
+	public String getTime();
+}
