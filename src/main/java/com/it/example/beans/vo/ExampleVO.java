74e8f4fffd201f750feb1e3b2125fb591c8211d3
jjaei <ieajnowk@gmail.com>
2023-04-16 14:53:41 +0900

    VO 예제

---
+++ src/main/java/com/it/example/beans/vo/ExampleVO.java
@@ -0,0 +1,12 @@
+package com.it.example.beans.vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class ExampleVO {
+	private String name;
+	private int age;
+}
