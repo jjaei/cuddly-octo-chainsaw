d20bc3e7b0c12bb056d6d2062d3cd2d3f288ccc7
jjaei <ieajnowk@gmail.com>
2023-04-16 14:54:51 +0900

    TimeMapper 인터페이스 수정

--- src/main/java/com/it/example/mappers/TimeMapper.java
+++ src/main/java/com/it/example/mappers/TimeMapper.java
@@ -1,12 +1,11 @@
 package com.it.example.mappers;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Service;
 
 @Service  // 스프링에서 관리하는 Bean에 등록
 @Mapper // 마이배티스 연결
 public interface TimeMapper {
-	@Select("select sysdate() from dual")
+//	@Select("select sysdate() from dual")
 	public String getTime();
 }
