e178da1f2a7ad534b3a0bab91fba919288ad1256
jjaei <ieajnowk@gmail.com>
2023-04-22 18:07:08 +0900

    게시글 목록 불러오기

---
+++ src/main/java/com/it/example/mappers/BoardMapper.java
@@ -0,0 +1,13 @@
+package com.it.example.mappers;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.it.example.beans.vo.BoardVO;
+
+@Mapper
+public interface BoardMapper {
+	// 게시글 목록
+	public List<BoardVO> getList();
+}
