0d01e3fd8dec0d1cd3fb11f81acfeeb7f4afc50f
jjaei <ieajnowk@gmail.com>
2023-04-23 12:21:20 +0900

    insert test 추가

--- src/test/java/com/it/example/mappers/BoardMapperTest.java
+++ src/test/java/com/it/example/mappers/BoardMapperTest.java
@@ -6,17 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.it.example.beans.vo.BoardVO;
+
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
 @Slf4j
 @RunWith( SpringJUnit4ClassRunner.class )
 public class BoardMapperTest {
+
 	@Autowired
 	private BoardMapper mapper;
 	
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board.toString()));
+		
+	}
+	
+//	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성");
+		board.setContent("새로 작성한 글의 내용");
+		board.setWriter("user01");
+		
+		mapper.insert(board);
+		
 	}
 }
