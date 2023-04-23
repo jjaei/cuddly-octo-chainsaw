78c1f515c52a8daa7e9989574de8ad6e9fe5fed2
jjaei <ieajnowk@gmail.com>
2023-04-23 16:46:12 +0900

    게시글 TEST

--- src/test/java/com/it/example/mappers/BoardMapperTest.java
+++ src/test/java/com/it/example/mappers/BoardMapperTest.java
@@ -32,6 +32,44 @@ public class BoardMapperTest {
 		board.setWriter("user01");
 		
 		mapper.insert(board);
+	}
+	
+//	@Test
+	public void testInsertSelectKey_bno() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성");
+		board.setContent("새로 작성한 글의 내용");
+		board.setWriter("user02");
 		
+		mapper.insertSelectKey_bno(board);	
+	}
+	
+//	@Test
+	public void testRead() {
+		long bno = 2L;
+		log.info(mapper.read(bno).toString());
+	}
+	
+//	@Test
+	public void testDelete() {
+		long bno = 3L;
+		log.info("[Delete Count] " + mapper.delete(bno));
+	}
+	
+	@Test
+	public void testUpdate() {
+		Long bno = 3L;
+		if(mapper.read(bno) != null) {
+			BoardVO board = new BoardVO();
+			// 실행 전에 존재하는 데이터 확인
+			board.setBno(1L);
+			board.setTitle("수정된 제목");
+			board.setContent("수정된 내용");
+			board.setWriter("user00");
+			
+			log.info("[Update Count] " + mapper.update(board));
+		} else {
+			log.info("[update Count] No Such Board!!! ");
+		}
 	}
 }
