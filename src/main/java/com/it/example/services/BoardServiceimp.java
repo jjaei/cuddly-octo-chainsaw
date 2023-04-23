a93e7939f74ba26a907ffdd24efd2722568291da
jjaei <ieajnowk@gmail.com>
2023-04-23 17:13:38 +0900

    Service 인터페이스 구현

---
+++ src/main/java/com/it/example/services/BoardServiceimp.java
@@ -0,0 +1,36 @@
+package com.it.example.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.it.example.beans.dao.BoardDAO;
+import com.it.example.beans.vo.BoardVO;
+
+@Service
+public class BoardServiceimp implements BoardService {
+	@Autowired
+	private BoardDAO boardDAO;
+	
+	@Override
+	public void register(BoardVO board) {
+		boardDAO.register(board);
+	};
+	@Override
+	public BoardVO get(Long bno) {
+		return boardDAO.get(bno);
+	};
+	@Override
+	public boolean modify(BoardVO board) {
+		return boardDAO.modify(board);
+	};
+	@Override
+	public boolean remove(Long bno) {
+		return boardDAO.remove(bno);
+	};
+	@Override
+	public List<BoardVO> getList(){
+		return boardDAO.getList();
+	};
+}
