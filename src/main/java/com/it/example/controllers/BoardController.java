3b271a9a3f75786f32939f836a7d85e54ab7c7fc
jjaei <ieajnowk@gmail.com>
2023-04-29 17:51:23 +0900

    board controller

--- src/main/java/com/it/example/controllers/BoardController.java
+++ src/main/java/com/it/example/controllers/BoardController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.it.example.beans.vo.BoardVO;
 import com.it.example.services.BoardService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,7 +26,7 @@ public class BoardController {
 	@GetMapping("list")
 	public void list(Model model) {
 		log.info("----------------------------");
-		log.info("[list()] ");
+		log.info("[BoardController] list() : ");
 		log.info("----------------------------");
 		model.addAttribute("list", service.getList());
 	}
@@ -41,5 +43,60 @@ public class BoardController {
 		
 		// RedirectView를 사용하면 redirect 방식으로 전송이 가능하다.
 		return new RedirectView("list");
+	}
+	
+	@GetMapping({"get", "get2"})
+	public void get(Long bno, HttpServletRequest request, Model model) {
+		String reqURI = request.getRequestURI();
+		String reqContextPath = request.getContextPath();
+		String reqType = reqURI.substring(reqURI.indexOf(reqContextPath) + 7);
+		log.info("----------------------------------------");
+		log.info("[BoardController] get : "  + reqURI);
+		log.info("[BoardController] get : "  + reqContextPath);
+		log.info("[BoardController] get : "  + reqType);
+		log.info("[BoardController] get : "  + bno);
+		log.info("----------------------------------------");
+		
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("modify")
+	public RedirectView modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("----------------------------------------");
+		log.info("[BoardController] modify : "  + board);
+		log.info("----------------------------------------");
+		if(service.modify(board) ){
+			//addAttribute()는 get 방식으로 QueryString에 전달
+			rttr.addFlashAttribute("result", "success");
+		} else {
+			rttr.addFlashAttribute("result", "failure");
+		}
+		
+		return new RedirectView("list");
+	}
+	
+	@PostMapping("remove")
+	// 변수명이 같은 경우 @RequestParam를 쓸 필요는 없으나 
+	// 명시적으로 지정할 경우 유지보수 중 파라미터 추가 시 발생할 수 있는 예외를 막아준다.
+	public RedirectView remove(@RequestParam("bno") Long bno, RedirectAttributes rttr)  {
+		log.info("----------------------------------------");
+		log.info("[BoardController] modify : "  + bno);
+		log.info("----------------------------------------");
+		
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		} else {
+			rttr.addFlashAttribute("result", "failure");
+		}
+		
+		return new RedirectView("list");
+	}
+	
+	@GetMapping("error")
+	public void error() throws Exception {
+		log.info("----------------------------");
+		log.info("[error()] ");
+		log.info("----------------------------");
+		throw new Exception("지울 자격도 없다.");
 	}
 }
