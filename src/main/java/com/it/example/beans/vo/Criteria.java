b2e12d72c52e278e7c90d109c0a276292b03e6e9
jjaei <ieajnowk@gmail.com>
2023-05-06 14:51:48 +0900

    criteria 생성

--- src/main/java/com/it/example/beans/vo/Criteria.java
+++ src/main/java/com/it/example/beans/vo/Criteria.java
@@ -9,15 +9,19 @@ import lombok.Data;
 public class Criteria {  // 게시글 검색의 기준
 	private int pageNum;   // 현재 몇 번째 페이지인가
 	private int amount;  	// 한 페이지에 보여질 게시글의 수
+	private int limit;
+	private int offset;
 	
 	public Criteria() {
-		this(1, 10);
-//		pageNum = 1;
+		this(0, 10);
+//		pageNum = 0;
 //		amount = 10;
 	}
 	
 	public Criteria(int pageNum, int amount) {
 		this.pageNum = pageNum;
 		this.amount = amount;
+		this.limit = pageNum * amount;
+		this.offset = (pageNum - 1) * amount;
 	}
 }
