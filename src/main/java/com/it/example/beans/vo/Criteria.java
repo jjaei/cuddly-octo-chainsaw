8d26019d8e1b660906bf2590055c74ae2131d6e7
jjaei <ieajnowk@gmail.com>
2023-05-06 12:42:20 +0900

    Criteria 추가

---
+++ src/main/java/com/it/example/beans/vo/Criteria.java
@@ -0,0 +1,23 @@
+package com.it.example.beans.vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component   
+@Data    // getter, setter 관리
+public class Criteria {  // 게시글 검색의 기준
+	private int pageNum;   // 현재 몇 번째 페이지인가
+	private int amount;  	// 한 페이지에 보여질 게시글의 수
+	
+	public Criteria() {
+		this(1, 10);
+//		pageNum = 1;
+//		amount = 10;
+	}
+	
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+}
