f979549a94612746fc667250a7c783c60dd54815
jjaei <ieajnowk@gmail.com>
2023-05-08 08:14:24 +0900

    댓글창을 위한 VO 생성

---
+++ src/main/java/com/it/example/beans/vo/ReplyVO.java
@@ -0,0 +1,18 @@
+package com.it.example.beans.vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class ReplyVO {
+	private Long rno;
+	private Long bno;
+	private String reply;
+	private String replier;
+	private String replydate;
+	private String updatedate;
+	
+	
+}
