f8092da54ce5e3c2c4402178256ef67cdafdf1bd
jjaei <ieajnowk@gmail.com>
2023-05-13 16:21:30 +0900

    Reply Service

---
+++ src/main/java/com/it/example/services/ReplyServiceImp.java
@@ -0,0 +1,52 @@
+package com.it.example.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.it.example.beans.dao.ReplyDAO;
+import com.it.example.beans.vo.Criteria;
+import com.it.example.beans.vo.ReplyVO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ReplyServiceImp implements ReplyService {
+	
+	@Autowired
+	private ReplyDAO replyDAO;
+	
+	@Override
+	public int register(ReplyVO reply) {
+		log.info("register..........." + reply);
+		return replyDAO.register(reply);
+	}
+	
+	@Override
+	public ReplyVO get(Long rno) {
+		log.info("get..........." + rno);
+		return replyDAO.get(rno);
+	}
+	
+	@Override
+	public int modify(ReplyVO reply) {
+		log.info("modify..........." + reply);
+		return replyDAO.modify(reply);
+	}
+	
+	@Override
+	public int remove(Long rno) {
+		log.info("remove..........." + rno);
+		return replyDAO.remove(rno);
+	}
+	
+	@Override
+	public List<ReplyVO> getList(Criteria cri, Long bno) {
+		log.info("getList..........." + bno);
+		return replyDAO.getList(cri, bno);
+	}
+}
