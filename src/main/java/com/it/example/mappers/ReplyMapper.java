accab2ca711c9f16095f1318983dd11fb8c01b19
jjaei <ieajnowk@gmail.com>
2023-05-13 12:21:27 +0900

    INSERT문 추가

--- src/main/java/com/it/example/mappers/ReplyMapper.java
+++ src/main/java/com/it/example/mappers/ReplyMapper.java
@@ -2,7 +2,10 @@ package com.it.example.mappers;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.it.example.beans.vo.ReplyVO;
+
 @Mapper
 public interface ReplyMapper {
-
+	// 댓글 등록
+	public int insert(ReplyVO reply);
 }
