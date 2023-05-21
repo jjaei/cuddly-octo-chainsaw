819ea5e4002a167525a3e9110e67bbb94f390856
jjaei <ieajnowk@gmail.com>
2023-05-21 16:45:07 +0900

    첨부파일 활용 VO 생성

---
+++ src/main/java/com/it/example/beans/vo/AttachFileVO.java
@@ -0,0 +1,14 @@
+package com.it.example.beans.vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class AttachFileVO {
+	private String fileName;
+	private String uploadPath;
+	private String uuid;
+	private boolean image;
+}
