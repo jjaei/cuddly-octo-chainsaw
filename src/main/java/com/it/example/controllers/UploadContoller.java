f0cbbad8bb5ba47cd0b6d69997e56ea1f95ef25d
jjaei <ieajnowk@gmail.com>
2023-05-22 17:01:38 +0900

    파일 업로드

--- src/main/java/com/it/example/controllers/UploadContoller.java
+++ src/main/java/com/it/example/controllers/UploadContoller.java
@@ -1,18 +1,30 @@
 package com.it.example.controllers;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.it.example.beans.vo.AttachFileVO;
 
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnailator;
@@ -57,18 +69,24 @@ public class UploadContoller {
 		return "/upload/result";  // 임시 리턴 페이지
 	}
 	
-	@PostMapping("uploadAjaxAction")
-	public String uploadAjaxPost(MultipartFile[] uploadFile, Model model) {
+//	@PostMapping("uploadAjaxAction")
+	@PostMapping(value= "uploadAjaxAction", produces= MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody  // **
+	public List<AttachFileVO> uploadAjaxPost(MultipartFile[] uploadFile, Model model) {
 		log.info("upload Ajax post! --------------------------------> ");
 		
+		List<AttachFileVO> fileList = new ArrayList<>();  // **
 		String uploadFolder = "C:/upload";
+		String uploadFolderPath = getFolder();
 		
 		// 1번 문제점 해결을 위해 /yyyy/mm/dd 경로를 생성
-		File uploadPath = new File(uploadFolder, getFolder());
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		log.info("upload path : " + uploadPath);
 		if(!uploadPath.exists()) {
 			uploadPath.mkdirs();
 		}
+		
+		log.info("upload path : " + uploadPath);
 		
 			for(MultipartFile f : uploadFile) {
 				// 받아온 f로 할 일
@@ -76,7 +94,14 @@ public class UploadContoller {
 				log.info("File Name : " + f.getOriginalFilename());
 				log.info("File Size : " + f.getSize());
 				
+				AttachFileVO attachFileVO = new AttachFileVO();
+				
+//				IE에서는 파일 이름이 포함한 전체 경로가 나오기 때문에 잘라주어야 한다.
 				String uploadFileName = f.getOriginalFilename();
+				uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+				log.info("file name(modified) " + uploadFileName);
+				
+				attachFileVO.setFileName(uploadFileName);  // **
 				
 				// UUID 적용
 				UUID uuid = UUID.randomUUID();
@@ -91,21 +116,31 @@ public class UploadContoller {
 				try {
 					// 파일 업로드 transferTo() 사용
 					f.transferTo(saveFile);
+					InputStream in = new FileInputStream(saveFile);  // **
+					attachFileVO.setUuid(uuid.toString());
+					attachFileVO.setUploadPath(uploadFolderPath);
+					
 					if(checkImageType(saveFile)) {  // 파일이 이미지라면
+						attachFileVO.setImage(true);  // **
 						FileOutputStream tn = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
 						// inputstream을 통해 파일을 생성하고 가로, 세로 크기를 지정해준다.
 						// 새로운 작은 파일은 s_ 가 해당 경로에 생성된다.
-						Thumbnailator.createThumbnail(f.getInputStream(), tn, 100, 100);
+						Thumbnailator.createThumbnail(in, tn, 32, 32);  // **
 						tn.close();
 					} else {
-						
+						attachFileVO.setImage(false);
 					}
+					
+					in.close();  // **
+					fileList.add(attachFileVO);  // **
+					
 				} catch(Exception e) {
 					log.error(e.getMessage());
 				}
 			}
 		
-		return "/upload/result";  // 임시 리턴 페이지
+//		return "/upload/result";  // 임시 리턴 페이지
+			return fileList;  // **
 	}
 	
 	private String getFolder() {
@@ -127,5 +162,22 @@ public class UploadContoller {
 			e.printStackTrace();
 		}
 		return false;
+	}
+	
+	@GetMapping("display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName){
+		log.info("getFile - filename : " + fileName + " ----------------------------- ");
+		File file = new File("C:/upload/" + fileName);
+		ResponseEntity<byte[]> result = null;
+		HttpHeaders header = new HttpHeaders();
+		try {
+			header.add("Content-type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 }
